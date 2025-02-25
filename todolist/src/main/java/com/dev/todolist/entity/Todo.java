d573905aa2be414ffaad1328bf0657aa3b758591
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2025-02-25 16:57:40 -0300

    Entidade Todo

---
+++ todolist/src/main/java/com/dev/todolist/entity/Todo.java
@@ -0,0 +1,48 @@
+package com.dev.todolist.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizada;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizada() {
+        return realizada;
+    }
+
+    public void setRealizada(boolean realizada) {
+        this.realizada = realizada;
+    }
+}
